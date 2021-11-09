ce97e6b12ad5bcf37af57c0645365053f22e0090
gml9812 <hee.jun.yun@gmail.com>
2021-11-09 22:56:38 +0900

    test: counter max,min 테스트

--- cypress/integration/counter.spec.js
+++ cypress/integration/counter.spec.js
@@ -28,5 +28,19 @@ describe("test counter", () => {
               cy.get("[data-cy=minus-button]").click();
               cy.get("[data-cy=counter]").should("have.value", initialCounterValue - 1);
           });
-    })
-})
+    });
+
+    it("counter max is 13", () => {
+        cy.get("[data-cy=plus-button]").click().click().click().click();
+        cy.get("[data-cy=counter]").should("have.value","13");
+    });
+
+    it("counter min is 7", () => {
+        //cy.get("[data-cy=minus-button]").click().click().click().click();
+
+        for(let i=0; i<10; i++){
+            cy.get("[data-cy=minus-button]").click();
+        }
+        cy.get("[data-cy=counter]").should("have.value","7");
+    });
+});
