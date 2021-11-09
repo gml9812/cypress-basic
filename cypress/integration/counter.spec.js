a972b6b1356aec3217b9eed7752c76bed19a419d
gml9812 <hee.jun.yun@gmail.com>
2021-11-09 21:48:51 +0900

    test: +,-버튼 클릭 시 카운터 증감 테스트

--- cypress/integration/counter.spec.js
+++ cypress/integration/counter.spec.js
@@ -2,11 +2,31 @@
 describe("test counter", () => {
     beforeEach(() => {
         cy.visit("/");
-    })
+    });
 
     it("render [+],[-] buttons and default counter value(10)", () => {
-        cy.get(".counter").should("have.value","10");
-        cy.get(".plus-button")
-        cy.get(".minus-button")
+        cy.get("[data-cy=counter]").should("have.value","10");
+        cy.get("[data-cy=plus-button]");
+        cy.get("[data-cy=minus-button]");
+    });
+
+    it("increases count when [+] button clicked", () => {
+        cy.get("[data-cy=counter]")
+          .then(($counterValue) => {
+              const initialCounterValue = Number($counterValue.val());
+              
+              cy.get("[data-cy=plus-button]").click();
+              cy.get("[data-cy=counter]").should("have.value", initialCounterValue + 1);
+          });
+    });
+
+    it("decreases count when [-] button clicked", () => {
+        cy.get("[data-cy=counter]")
+          .then(($counterValue) => {
+              const initialCounterValue = Number($counterValue.val());
+              
+              cy.get("[data-cy=minus-button]").click();
+              cy.get("[data-cy=counter]").should("have.value", initialCounterValue - 1);
+          });
     })
 })
--- src/js/components/Counter.js
+++ src/js/components/Counter.js
@@ -3,9 +3,9 @@ export default function Counter({ $app }) {
         $app.innerHTML = `
         <div class="container">
           <h1>cypress counter</h1>
-          <button class="plus-button">+</button>
-          <input name="counter" class="counter" value="10" />
-          <button class="minus-button">-</button>
+          <button class="plus-button" data-cy="plus-button">+</button>
+          <input class="counter" data-cy="counter" value="10" />
+          <button class="minus-button" data-cy="minus-button">-</button>
         </div>
         `
     };
